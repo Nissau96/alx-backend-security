f7baa5e8846e0ebb1c161dc57ef7eefd03821cd4
Nissau96 <ihalidu996@gmail.com>
2025-09-04 13:50:57 +0000

    feat: add SuspiciousIP model for anomaly flagging

--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -27,3 +27,15 @@ class BlockedIP(models.Model):
     def __str__(self):
         return self.ip_address
 
+class SuspiciousIP(models.Model):
+    ip_address = models.CharField(max_length=45, unique=True)
+    reason = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [models.Index(fields=['ip_address'])]
+        verbose_name = "Suspicious IP"
+        verbose_name_plural = "Suspicious IPs"
+
+    def __str__(self):
+        return f"{self.ip_address} - {self.reason}"
